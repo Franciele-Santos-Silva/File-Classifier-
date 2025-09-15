c8d121aad4a391f9a3f651352abbd1bc30d37036
Franciele-Santos-Silva <franciellysantos876@gmail.com>
2025-09-15 18:46:22 -0300

    Adicionando tipos de arquivos

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 import os
 from tkinter.filedialog import askdirectory
 
+caminho = askdirectory(title="Selecione uma pasta")
+
+lista_arquivos = os.listdir(caminho)
+print(lista_arquivos)
+
+locais = {
+    "imagens": [".png", ".jpg", ".jpeg", ".gif", ".bmp", ".tiff"],
+    "documentos": [".pdf", ".doc", ".docx", ".txt", ".xls", ".xlsx", ".ppt", ".pptx"],
+    "audio": [".mp3", ".wav", ".aac", ".flac", ".ogg"],
+    "videos": [".mp4", ".avi", ".mov", ".mkv", ".flv"],
+    "compactados": [".zip", ".rar", ".tar", ".gz", ".7z"],
+    "outros": []
+}
